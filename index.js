8472fca3dcc5cb210776b4513fdddf72280ff50e
Sara T <sahara.tijol@gmail.com>
2020-09-24 15:55:48 -0500

    Feat: Fully Functional Delete Button

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const getUrl = 'https://stupendous-expensive-domain.glitch.me/movies';
 const getMovies = () => fetch(getUrl)
     .then(res => res.json())
@@ -5,24 +7,22 @@ const getMovies = () => fetch(getUrl)
         let renderMovie = ''
         for (let movie of movies) {
             renderMovie += `<li class="card">
-                                         <p>${movie.title}</p>
-                                         <p>${movie.id}</p>
-                                         <button class="editMovie">Edit Movie</button>
-                                         <button class="deleteMovie" data-id=${movie.id}>Delete Movie</button></li>`
-            // <button class="deleteMovie" onclick="$(this).getMovieID(${movie.id})" data-id=${movie.id}>Delete Movie</button></li>`
+                            <p>${movie.title}</p>
+                            <p>${movie.id}</p>
+                            <button class="editMovie">Edit Movie</button>
+                            <button onclick="deleteMovie(this)" data-id=${movie.id}>Delete Movie</button></li>`
             $('#result').html(renderMovie)
         }
     })
-
 
 document.onreadystatechange = function () {
     if (document.readyState === "complete") {
         $('#loader').remove()
         getMovies()
-
-
     }
 };
+
+
 
 const addMovie = () => {
     let movie = {title: $('#title').val(), genre: $('#genre').val(), rating: $('#rating').val()};
@@ -32,58 +32,47 @@ const addMovie = () => {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(movie)
-    })
+        })
         .then((data) => {
             getMovies()
         })
         .catch(console.error);
 }
 
-const deleteMovie = (id) => fetch(`${getUrl}/${id}`, {
+
+const deleteMovie = (ele) => {
+    let dataID = $(ele).data('id')
+    fetch(`${getUrl}/${dataID}`, {
     method: 'DELETE',
     headers: {
         'Content-Type': 'application/json'
     }
+    })
+    .then((data) => {
+        getMovies()
+    })
+    .catch(console.error);
+    console.log(dataID)
+}
+
+const editMovies = movie => fetch(`${getUrl}/${id}`, {
+    method: 'PUT',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(movie) //convert json into a string to be placed in the body. Very important
 })
     .then(res => res.json())
-    .then(() => {
-        getMovies()
+    .then(data => {
+        console.log(`Success: edited ${JSON.stringify(data)}`);
     })
     .catch(console.error);
 
 
-/*--------- Event Listeners --------*/
+/*---------- Event Listeners ---------*/
 $('#addMovie').click((e) => {
     e.preventDefault()
     addMovie();
 })
 
 
-// <button class="deleteMovie" onclick="$(this).getMovieID(${movie.id})"
-// data-id=${movie.id}>Delete Movie</button></li>`
-$.fn.getMovieID = function (movieID) {
-    console.log(movieID)
-    deleteMovie(movieID)
-    // editMovie()
-}
-
-
-// $('.deleteMovie').each(function() {
-//     $(this).click(function () {
-//         // let movieId = $(this).data('data-id').val()
-//         console.log('movieId')
-//         // deleteMovie(movieId)
-//     })
-// })
-//$('.deleteMovie').click($(this).getMovieID())
-
-// $('.deleteMovie').click(function() {
-//     var id = $(this).attr('data-id')
-//     console.log(id)
-// })
-
-// $(document).ready(function () {
-//     $('.deleteMovie').click(function () {
-//         alert(this.data - id);
-//     })
-// })
