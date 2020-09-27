7b4f14f7b20e93289a960a1bb0fd498c60c0c0f3
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-09-27 17:56:37 -0500

    Feat: functional delete button

--- index.js
+++ index.js
@@ -4,30 +4,23 @@ const getUrl = 'https://stupendous-expensive-domain.glitch.me/movies';
 const getMovies = () => fetch(getUrl)
     .then(res => res.json())
     .then(movies => {
-        //console.log(movies)
+        console.log(movies)
         let renderMovie = ''
+        if (movies.length === 0)
+           $('#result').html('')
+        else
         for (let movie of movies) {
             let grabTitle = movie.title;
             let grabGenre = movie.genre;
             let grabID = movie.id;
             renderMovie +=
                 `<div class="movieFromAPI mx-3">
-                        <button onclick="renderModal(this)" data-title="${grabTitle}" data-genre=${grabGenre} data-rating=${movie.rating} data-id=${grabID} data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap">Edit Movie</button>
+                        <button class="mt-5" onclick="renderModal(this)" data-title="${grabTitle}" data-genre=${grabGenre} data-rating=${movie.rating} data-id=${grabID} data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap">Edit Movie</button>
                         <button onclick="deleteMovie(this)" data-id=${grabID}>Delete Movie</button>
                         <img id="movieImg" src=${posterUrl + movie.poster_path} data-movie-id=${movie.id}/>
                     </div>`
-            //PULLED BUTTON FROM COPY AND PASTE FROM RENDERMOVIE;
-                // <button id="addMovie" onclick="addMovie(this)" data-movie-id= class="mr-4 mt-3"><i class="fas fa-plus"></i></button>
 
-            //Original rendering of movies from database
-            // `<li class="card">
-            //     <p>${movie.title}</p>
-            //     <p>${movie.id}</p>
-            //     <button onclick="renderModal(this)" data-title="${grabTitle}" data-genre=${grabGenre} data-rating=${movie.rating} data-id=${movie.id} data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap">Edit Movie</button>
-            //     <button onclick="deleteMovie(this)" data-id=${movie.id}>Delete Movie</button>
-            //  </li>`
-
-            $('#result').html(renderMovie)
+            $('#result').html(renderMovie);
         }
 
     })
@@ -38,24 +31,6 @@ document.onreadystatechange = function () {
         getMovies()
     }
 };
-
-
-// const addMovie = () => {
-//     let movie = {title: $('#title').val(), genre: $('#genre').val(), rating: $('#rating').val()};
-//     fetch(`${getUrl}`, {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(movie)
-//     })
-//         .then((data) => {
-//             getMovies()
-//         })
-//         .catch(console.error);
-// }
-
-
 
 const deleteMovie = (ele) => {
     let dataID = $(ele).data('id')
@@ -142,17 +117,6 @@ const editMovie = (ele) => {
 }
 
 
-/*---------- Event Listeners ---------*/
-// $('#addMovie').click((e) => {
-//     e.preventDefault()
-//     // addMovie();
-//     console.log("I've been clicked!")
-// })
-
-
-
-
-
 /*-------------------------------- themoviedb.org API Tests ----------------------------------*/
 const posterUrl = 'https://image.tmdb.org/t/p/w500';
 const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + themoviedb_API;
@@ -235,12 +199,10 @@ function getPopularMovies(value) {
 
 function addMovie(ele) {
     let movieID = $(ele)[0].attributes[2].nodeValue;
-    // console.log(movieID) //will log the specific movie ID
 
     const path = `/movie/${movieID}`
     const url = generateUrl(path);
 
-    console.log(url) //will log url that will get movie info.
     fetch(url)
         .then((res) => res.json())
         .then((data) => {
