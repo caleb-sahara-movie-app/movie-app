37a0aab6ed0173fcb5cc5d293f659661e166ec5e
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-12-06 18:41:10 -0600

    feat: organized index.js and have a functioning edit movie feature

--- index.js
+++ index.js
@@ -1,37 +1,62 @@
 'use strict';
 
-const getUrl = 'https://stupendous-expensive-domain.glitch.me/movies';
+/*--------------------------- All functions associated with database info: 5-132 -----------------------------*/
+
+//Get Information from our database (WATCHLIST) and display a Movie object in our watchlist
+const getUrl = 'https://goldenrod-spotty-woolen.glitch.me/movies';
 const getMovies = () => fetch(getUrl)
     .then(res => res.json())
     .then(movies => {
-        // console.log(movies[0].vote_average)
-        // console.log(movies)
+
         let renderMovie = ''
         if (movies.length === 0)
             $('#result').html('')
             //  $('.modal-dialog').html('')
         else
             for (let movie of movies) {
+                console.log(movies)
                 let grabID = movie.id;
                 renderMovie +=
                     `<div class="movieFromAPI mx-3">
                         <button class="btn btn-danger" onclick="deleteMovie(this)" data-id=${grabID}>Delete Movie</button>
-                        <button class="btn btn-primary mt-5" onclick="renderModal(this)" data-rating=${movie.vote_average} data-id=${grabID} data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap">Edit Movie</button>
+                        <button class="btn btn-primary mt-5" onclick="renderModal(${movie.id}, ${movie.vote_average})" data-rating=${movie.vote_average} data-id=${grabID} data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap">Edit Movie</button>
                         <img id="movieImg" src=${posterUrl + movie.poster_path} data-movie-id=${movie.id}/>
                     </div>`
 
                 $('#result').html(renderMovie);
-                // $('.modal-dialog').html(renderMovie);
             }
     })
 
+// Loader function that will display a loading message until our movies are retrieved from database
 document.onreadystatechange = function () {
     if (document.readyState === "complete") {
         $('#loader').remove()
         getMovies()
     }
 };
-
+// Function that will handle adding a movie to database
+function addMovie(ele) {
+    let movieID = $(ele)[0].attributes[3].nodeValue; //Attribute for ID has moved indexes
+    const path = `/movie/${movieID}`
+    const url = generateUrl(path);
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            let addedMovie = data;
+            console.log(addedMovie) //logs clicked movie data
+            fetch(`${getUrl}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(addedMovie)
+            })
+                .then((data) => data.json())
+                .then(getMovies)
+                .catch(console.error);
+        })
+}
+// Function that will handle deleting a movie from database
 const deleteMovie = (ele) => {
     let dataID = $(ele).data('id')
     fetch(`${getUrl}/${dataID}`, {
@@ -45,16 +70,27 @@ const deleteMovie = (ele) => {
     console.log(dataID)
 }
 
+// Function that will handle editing a movie in the database
+const editMovie = (id, rating) => {
 
-const renderModal = (ele) => {
+    let updateAverage = {'vote_average': rating}
+    fetch(`${getUrl}/${id}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(updateAverage)
+    })
+        .then((res => res.json()))
+        .then((data) => {
+            getMovies()
+        })
+        .catch(console.error);
+}
 
-    console.log(ele)
-    console.log($(ele)[0].attributes[2])
-
-    // console.log($(ele)[0].attributes[3])
-    let dataID = [($(ele)[0].attributes[2].nodeValue), ($(ele)[0].attributes[3].value)]
-    // let userRating = $('#rating-change').val();
-
+// Function that will render a modal so a user can edit a movie in the database
+const renderModal = (id, rate) => {
+    console.log("id: "+id+ ", rating: "+ rate)
     let createModal = ''
     createModal +=
         `<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -69,7 +105,7 @@ const renderModal = (ele) => {
             <div class="modal-body">
                 <form class="w-100">
                     <div class="form-group m-3">
-                        <label for="rating-change" class="col-form-label">Current Rating: ${dataID[0]}</label>
+                        <label for="rating-change" class="col-form-label">Current Rating: ${rate}</label>
                         <select class="form-control" id="rating-change">
                             <option selected="true" disabled>Please select a rating:</option>
                             <option value="1">1</option>
@@ -87,8 +123,8 @@ const renderModal = (ele) => {
                 </form>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal2">Close</button>
-                <button id="rating" type="button" onClick="editMovie(this)" data-id=${dataID[1]} data-rating="" class="updateChange btn btn-primary">Submit Changes</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal2">Close</button>         
+                <button class="updateChange btn btn-primary" data-rating="" data-toggle="" id="rating" type="submit">Submit Changes</button>
             </div>
         </div>
     </div>
@@ -97,191 +133,56 @@ const renderModal = (ele) => {
     $('.putModal').html(createModal)
 
     let rating = document.getElementById('rating-change').value;
-    // console.log(rating)
+    console.log(rating)
     $('#rating-change').change(function () {
         rating = '';
-
         rating = ($(this).val())
-        // console.log(rating)
-        $('#rating').attr({'data-rating': `${rating}`})
+        $('#rating').attr('data-rating', rating);
     })
+
+    $(document).on('click', '#rating', function(){
+        let rating = $('#rating').attr('data-rating');
+        console.log("updated value: "+rating)
+        console.log("id: "+ id)
+        editMovie(id, rating)
+    });
+
     $("button[data-dismiss=modal2]").click(function(){
         $('#exampleModal').modal('hide');
     })
 }
-
-/*-------------------------------- MODAL CLOSE OVERRIDE ----------------------------------*/
+// Edit modal close override
 $("button[data-dismiss=modal1]").click(function(){
     $('#myModal').modal('hide');
-    console.log("hi there")
 });
 
-/*-------------------------------- themoviedb.org API Tests ----------------------------------*/
+
+/*-------------------------------- Information That Will Display Movies To Landing Page ----------------------------------*/
+//          URL info for API Requests
 const posterUrl = 'https://image.tmdb.org/t/p/w500';
 const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + themoviedb_API;
-
-// Movie Poster Function
-function movieSection(movies) {
-            let renderMovie = ''
-            for (let movie of movies) {
-                if (movie.poster_path) {
-                    let grabID = movie.id;
-                    renderMovie +=
-                        `<div class="movieFromAPI mx-3">
-                        <button class="btn btn-success" id="addMovie" onclick="addMovie(this)" data-movie-id=${grabID} class="mr-4 mt-3"><i class="far fa-heart"></i></button>
-                        <img id="movieImg" src=${posterUrl + movie.poster_path} data-movie-id=${movie.id}/>
-                    </div>`
-                }
-            }
-            return renderMovie;
-}
-
-// Select Elements from DOM
-const moviesContainer = document.querySelector('#movies-container');
-
-
-/*------------------ API requests -----------------*/
-
 function generateUrl(path) {
     const url = `https://api.themoviedb.org/3${path}?api_key=${themoviedb_API}`;
     return url;
 }
-
-// Responsible whenever we request new movies
+//          Will request Movie info based on URL path
 function requestMovies(url, onComplete, onError) {
     fetch(url)
         .then((res) => res.json())
+        .then((data) => {
+            console.log(data)
+            return data;
+        })
         .then(onComplete)
         .catch(onError);
 }
-
 // Some Basic Error Handler
 function handleError(error) {
     console.log('Error:', error);
 }
 
-function getPopularMovies(value) {
-    const path = '/movie/popular';
-    const url = generateUrl(path);
 
-    const render = renderMovies.bind({title: 'Popular Movies'})
-    requestMovies(url, render, handleError);
-}
-
-
-function searchMovies(value) {
-    const path = '/search/movie';
-    const url = generateUrl(path) + '&query=' + value;
-
-    requestMovies(url, renderSearchMovies, handleError);
-}
-
-function getTrendingMovies(value) {
-    const path = '/trending/movie/week';
-    const url = generateUrl(path);
-
-    const render = renderMovies.bind({title: "What's Trending?"})
-    requestMovies(url, render, handleError);
-}
-
-function getTopRatedMovies(value) {
-    const path = '/movie/top_rated';
-    const url = generateUrl(path);
-
-    const render = renderMovies.bind({title: 'Top Rated Movies'})
-    requestMovies(url, render, handleError);
-}
-
-function getPopularMovies(value) {
-    const path = '/movie/popular';
-    const url = generateUrl(path);
-
-    const render = renderMovies.bind({title: 'Popular Movies'})
-    requestMovies(url, render, handleError);
-}
-
-function getUpcomingMovies(value) {
-    const path = '/movie/upcoming';
-    const url = generateUrl(path);
-
-    const render = renderMovies.bind({title: 'Upcoming'})
-    requestMovies(url, render, handleError);
-}
-
-function addMovie(ele) {
-    let movieID = $(ele)[0].attributes[3].nodeValue; //Attribute for ID has moved indexes
-    const path = `/movie/${movieID}`
-    const url = generateUrl(path);
-
-    fetch(url)
-        .then((res) => res.json())
-        .then((data) => {
-            let addedMovie = data;
-            console.log(addedMovie) //logs clicked movie data
-            fetch(`${getUrl}`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(addedMovie)
-            })
-                .then((data) => data.json())
-                .then(getMovies)
-                // getMovies()
-
-                .catch(console.error);
-        })
-
-}
-
-const editMovie = (ele) => {
-    console.log($(ele))
-    let rating = {'vote_average': ($(ele)[0].attributes[4].nodeValue)}
-    let dataID = ($(ele)[0].attributes[3].nodeValue)
-    fetch(`${getUrl}/${dataID}`, {
-        method: 'PATCH',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(rating)
-    })
-        .then((res => res.json()))
-        .then((data) => {
-            getMovies()
-
-        })
-        .catch(console.error);
-}
-/*---------- Render to HTML -----------*/
-
-// Trying to figure out how convert vanilla JS to jQuery when adding new elements to document
-function createMovieContainer(movies, title = '') {
-    const movieElement = document.createElement('div');
-    movieElement.setAttribute('class', 'movie');
-
-    const movieTemplate = `
-     <h2>${title}</h2>
-     <section class="section"> 
-        ${movieSection(movies)}
-      </section>
-       `;
-    // Notice we're calling that function to feed into <section> : movieSection() on ln.146
-    movieElement.innerHTML = movieTemplate;
-    return movieElement;
-}
-
-
-// We use this to populate upcoming, popular, and top-rated movies w/ different url paths
-function renderMovies(data) {
-    // data.results []
-    const movies = data.results;
-    const movieBlock = createMovieContainer(movies, this.title);
-    $('#movies-container').append(movieBlock)
-    // console.log('Data:', data);
-}
-
-
-// Listener for the Search submit button
+//------------------------------------ Event Listener and Functions for Searching -------------------------//
 $('#search-result').hide()
 $('#search-input').keyup((e) => {
     $('#movies-container').hide()
@@ -294,8 +195,6 @@ $('#search-input').keyup((e) => {
     }
     searchMovies(value);
 });
-
-// Render the result of searched movies
 function renderSearchMovies(data) {
     console.log(data.results)
     if (data.results[0].id === 680304) {
@@ -307,42 +206,105 @@ function renderSearchMovies(data) {
         $('#search-result').append(movieBlock);
     }
 }
+function searchMovies(value) {
+    const path = '/search/movie';
+    const url = generateUrl(path) + '&query=' + value;
+    requestMovies(url, renderSearchMovies, handleError);
+}
 
-// Sort By dropdown
+
+//------------------------------------ Event Listener and Function forEach Sorting Value -------------------------//
+// Upcoming Movies
 $('#upcoming').click(function() {
     getUpcomingMovies();
     $('#movies-container').html('')
 })
+function getUpcomingMovies(value) {
+    const path = '/movie/upcoming';
+    const url = generateUrl(path);
+    const render = renderMovies.bind({title: 'Upcoming'})
+    requestMovies(url, render, handleError);
+}
 
+// Trending Movies
 $('#trending').click(function() {
     getTrendingMovies();
     $('#movies-container').html('');
 })
+function getTrendingMovies(value) {
+    const path = '/trending/movie/week';
+    const url = generateUrl(path);
+    const render = renderMovies.bind({title: "What's Trending?"})
+    requestMovies(url, render, handleError);
+}
 
+// Popular Movies
 $('#popular').click(function() {
     getPopularMovies();
     $('#movies-container').html('');
 })
+function getPopularMovies(value) {
+    const path = '/movie/popular';
+    const url = generateUrl(path);
+    const render = renderMovies.bind({title: 'Popular Movies'})
+    requestMovies(url, render, handleError);
+}
 
+// Top Rated Movies
 $('#rating').click(function() {
     getTopRatedMovies();
     $('#movies-container').html('')
 })
+function getTopRatedMovies(value) {
+    const path = '/movie/top_rated';
+    const url = generateUrl(path);
+    const render = renderMovies.bind({title: 'Top Rated Movies'})
+    requestMovies(url, render, handleError);
+}
 
-// Genres
+//---------------------------------- This function will take passed values from event listeners ------------------------------//
+//---------------------------- and send it to createMovieContainer() to build a movie section in HTML ------------------------------//
+function renderMovies(data) {
+    const movies = data.results;
+    const movieBlock = createMovieContainer(movies, this.title);
+    $('#movies-container').append(movieBlock)
+}
 
+// Will generate a Container for passed Movies to be placed in HTML
+function createMovieContainer(movies, title = '') {
+    const movieElement = document.createElement('div');
+    movieElement.setAttribute('class', 'movie');
+
+    const movieTemplate = `
+     <h2>${title}</h2>
+     <section class="section"> 
+        ${movieSection(movies)}
+      </section>`;
+    movieElement.innerHTML = movieTemplate;
+    return movieElement;
+}
+
+// Will generate a Movie Section that contains each Movie from passed movies object. Will be passed into createMovieContainer()
+function movieSection(movies) {
+    let renderMovie = ''
+    for (let movie of movies) {
+        if (movie.poster_path) {
+            let grabID = movie.id;
+            renderMovie +=
+                `<div class="movieFromAPI mx-3">
+                        <button class="btn btn-success" id="addMovie" onclick="addMovie(this)" data-movie-id=${grabID} class="mr-4 mt-3"><i class="far fa-heart"></i></button>
+                        <img id="movieImg" src=${posterUrl + movie.poster_path} data-movie-id=${movie.id}/>
+                    </div>`
+        }
+    }
+    return renderMovie;
+}
 
 /*--------- Invoke API Requests ----------*/
+getTrendingMovies();
 searchMovies();
 
-getTrendingMovies();
 
-//getPopularMovies();
-
-// getTopRatedMovies();
-
-
-/* TOP NAVBAR*/
 
 
 
